561ba93b0fcde74b4b07d890bdb04ed80df1d596
mahesh <147mahesh258@gmail.com>
2024-03-14 10:23:40 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -3,12 +3,14 @@
 import { request } from "express";
 
 const asyncHandler = (requesthandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise
     .resolve(requesthandler(req, res, next))
     .catch((err) => next(err));
   };
 };
+
+export {asyncHandler}
 
 //using async await
 
