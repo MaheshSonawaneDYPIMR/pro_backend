3f5864188331f76d6e15bdeb9132eb56bc997e93
mahesh <147mahesh258@gmail.com>
2024-03-14 12:00:12 +0530

    register controller added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,68 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponce } from "../utils/ApiResponce.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message:'okk'
+  const { fullName, email, userName, password } = req.body;
+  if (
+    [fullName, email, userName, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "Please fill all the fields");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ userName }, { email }],
   });
+  if (existedUser) {
+    throw new ApiError(409, "User already exists");
+  }
+
+  const avatarLocalPath = req.filles?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(400, "Avatar upload failed");
+  }
+
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    userName: userName.toLowerCase(),
+    password,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "something went wrong while registering the user");
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponce(201, createdUser, "User created successfully"));
 });
 
 export { registerUser };
+
+//get user details from frontend
+//validation
+//check if user is already registered
+//check for images and avatar images
+//upload them to cloudinary
+//create user object - create entry in db
+//remove password and refresh tokenfrom response
+//check for user creation
+//return responce
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -53,7 +53,7 @@ const userSchema = new mongoose.Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
@@ -91,4 +91,4 @@ userSchema.methods.generateRefreshToken = async function () {
 }
 
 
-const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
